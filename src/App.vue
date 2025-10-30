c77b45baafae5b0603bcf7cb63bfebb3a03bad15
RodRR <rodolforomerovg@gmail.com>
2025-10-30 14:09:30 -0600

    fondo de prueba

--- src/App.vue
+++ src/App.vue
@@ -19,5 +19,9 @@ export default{
 </template>
 
 <style>
-
+  body{
+    background: #000000;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #04AA6D, #434343);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #04AA6D, #434343); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
 </style>
